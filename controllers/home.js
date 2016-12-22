cd40bde3d9b0f2481d61d8b87fa3b49d644170b0
Keskinev87 <yavor_keskinev@abv.bg>
2016-12-22 18:05:46 +0200

    Add files via upload

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,17 @@
+const Mongoose = require('mongoose')
+const Transaction = Mongoose.model('Transaction')
+
 module.exports = {
   index: (req, res) => {
-      res.render('home/index');
+      Transaction.find(Transaction.date = Date.now()).then(sales =>{
+          let total = 0;
+          for (let sale of sales){
+              total += Number(sale.price)
+          }
+          let salesCount = sales.length
+          res.render('home/index', {salesCount: salesCount,total: total });
+      })
+
+
   }
 };
